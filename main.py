3c2e4e52f23f11d120b7c81bd0104d452b27ee09
W1nkobot <n0s4dm3n_chel@mail.ru>
2023-09-12 11:27:26 +0300

    something

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def input_check(num_sys):
-    if num_sys == "2" or "3" or "8" or "10" or "16":
-        return 1
+def input_check():
+    num_sys = input()
+    if num_sys == '2' or '3' or '8' or '10' or '16':
+        return True
     else:
-        return 0
+        return False
 
 
 def output_check(num_sys):
@@ -12,20 +13,28 @@ def output_check(num_sys):
         return 0
 
 
+def second_step():
+    print("Now choose the output number system")
+    print("2, 3, 8, 10 or 16")
+    output_num_sys = input()
+    if output_check(output_num_sys):
+        print("Yo, nice")
+    else:
+        print("There's some problem with number system you put, try again")
+
+
 def first_step():
     print("Hello user! Lets make some deal on that numbers")
     print("Choose the number system first")
     print("2, 3, 8, 10 or 16")
-    input_num_sys = input()
-
-    if input_check(input_num_sys) == 1:
-
-        print("Now choose the output number system")
-        print("2, 3, 8, 10 or 16")
-        output_num_sys = input()
-        if output_check(output_num_sys) == 1:
-            print("Yo, nice")
-        else:
-            print("There's some problem with number system you put, try again")
-    else:
+    if not input_check():
         print("There's some problem with number system you put, try again")
+    else:
+        second_step()
+
+
+def repeat():
+    first_step()
+
+
+repeat()
