9dfb333cf208b6e36b13d078b6c6fb6324747749
W1nkobot <n0s4dm3n_chel@mail.ru>
2023-09-14 10:13:34 +0300

    something

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+def trinity_choose():
+    print("Choose one of trinity number system: Symmetric or Asymmetric")
+    trinity = input()
+    if trinity == "Symmetric":
+        print("Chosen Symmetric")
+
+    elif trinity == "Asymmetric":
+        print("Chosen Asymmetric")
+    else:
+        print("Somethng goes wrong")
+        trinity_choose()
+
+
 def input_check():
     num_sys = input()
-    if num_sys == '2' or '3' or '8' or '10' or '16':
+    if num_sys == '2':
         return True
+    if num_sys == '3':
+        trinity_choose()
+    if num_sys == '8':
+        return True
+    if num_sys == '10':
+        return True
+    if num_sys == '16':
+        return True
+
     else:
         return False
 
